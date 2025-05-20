a79f47d0e8669fecc309549864e85ee0f626b4d1
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2025-05-20 22:05:30 +0900

    Initial Commit

---
+++ Sources/ClockUI/ClockView.swift
@@ -0,0 +1,43 @@
+//
+//  ClockView.swift
+//  ClockUI
+//  
+//  https://github.com/RyoDeveloper/ClockUI
+//  Copyright © 2025 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClockView: View {
+    var body: some View {
+        VStack {
+            TimelineView(.periodic(from: Date(), by: 1)) { timeline in
+                Group {
+                    Text(
+                        timeline.date,
+                        format: Date.FormatStyle()
+                            .year()
+                            .month()
+                            .day()
+                            .weekday(.abbreviated)
+                    )
+
+                    Text(
+                        timeline.date,
+                        format: Date.FormatStyle()
+                            .hour()
+                            .minute()
+                            .second()
+                    )
+                }
+                .font(.title)
+                .monospacedDigit()
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ClockView()
+}
